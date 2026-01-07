76e8e78185e1d23100cdb03f992b166c88339def
Tina Gahan <gahantina@gmail.com>
2026-01-07 22:18:32 +0530

    Remove experimental files from rep tracking

--- logic.py
+++
@@ -1,7 +0,0 @@
-temperature = 38 #celsius
-soil_moisture = 20 #in percentage
-
-if temperature > 35 and soil_moisture < 30:
-	print("crop is stressed : water deficiency")
-else:
-	print("crop is healthy!")
